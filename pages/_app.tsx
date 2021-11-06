747f4ed5e2353219b7dcc9e7e523ec94ac763d02
einemutigeschlacht <eine.schlacht@gmail.com>
2021-11-06 13:49:33 +0700

    :tada:: new(currently): adding logo for web.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from "next/head"
 import "tailwindcss/tailwind.css";
 import "./../styles/_global.css";
 
@@ -11,9 +11,16 @@ function MyApp({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL} />
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="Faculty of Technology, Phanomsarakham Technology" />
+                <meta property="og:title" content="เว็บไซต์หมวดหมู่เทคโนโลยี, วิทยาลัยเทคนิคพนมสารคาม" />
                 <meta property="og:description" content="เว็บไซต์สาขาอาชีพที่เกี่ยวกับเทคโนโลยีในวิทยาลัยเทคนิคพนมสารคาม" />
                 <meta property="og:image" content={"https://i.imgur.com/ZmQUyfO.jpg"} />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/manifest.json" />
             </Head>
             <Component {...pageProps} />
         </>
